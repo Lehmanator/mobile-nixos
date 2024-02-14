748615a620e70195c9864bbb465c7102ae666d59
Sam Lehman <github@samlehman.dev>
2024-02-14 03:21:45 -0500

    update alsa-ucm-conf to latest

--- devices/families/sdm845-mainline/sound.nix
+++ devices/families/sdm845-mainline/sound.nix
@@ -1,33 +1,31 @@
-{ config, lib, pkgs, ... }:
-
 {
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   nixpkgs.overlays = [
     (self: super: {
-      sdm845-alsa-ucm = self.callPackage
-        (
-          { runCommand, fetchFromGitLab }:
-
-          runCommand "sdm845-alsa-ucm"
-            {
-              src = fetchFromGitLab {
-                name = "sdm845-alsa-ucm";
-                owner = "sdm845-mainline";
-                repo = "alsa-ucm-conf";
-                rev = "08cbf26e684f5609b3839870887c81be378712f6"; # master
-                sha256 = "sha256-ZvknDKjcLyYwlFuqLTOE91GAQM7RykL8flGgYunttS8=";
-              };
-            } ''
-            mkdir -p $out/share/
-            ln -s $src $out/share/alsa
-          ''
-        )
-        { };
+      sdm845-alsa-ucm = self.callPackage ({
+        runCommand,
+        fetchFromGitLab,
+      }:
+        runCommand "sdm845-alsa-ucm" {
+          src = fetchFromGitLab {
+            name = "sdm845-alsa-ucm";
+            owner = "sdm845-mainline";
+            repo = "alsa-ucm-conf";
+            rev = "c5a680c08b39d4a665479c9bd30481a137b60cc4"; # Branch: sdm845-phones (2024-02-14)
+            sha256 = "sha256-s4S4ULVkxGPAm0+0xzbvxWDX695XpiKWOp06/oMyU9s=";
+          };
+        } ''
+          mkdir -p $out/share/
+          ln -s $src $out/share/alsa
+        '') {};
     })
   ];
 
   # Alsa UCM profiles
   mobile.quirks.audio.alsa-ucm-meld = true;
-  environment.systemPackages = [
-    pkgs.sdm845-alsa-ucm
-  ];
+  environment.systemPackages = [pkgs.sdm845-alsa-ucm];
 }
