5f675b8ff2504021d042d0dddf6b30f8d11b5d89
Sam Lehman <github@samlehman.dev>
2024-02-14 03:13:05 -0500

    update sdm845-mainline kernel to 6.7.4

--- devices/families/sdm845-mainline/kernel/default.nix
+++ devices/families/sdm845-mainline/kernel/default.nix
@@ -1,18 +1,19 @@
-{ mobile-nixos
-, fetchFromGitLab
-, fetchpatch
-, ...
+{
+  mobile-nixos,
+  fetchFromGitLab,
+  fetchpatch,
+  lib,
+  ...
 }:
-
 mobile-nixos.kernel-builder {
-  version = "6.7.0-rc5";
+  version = "6.7.4";
   configfile = ./config.aarch64;
 
   src = fetchFromGitLab {
     owner = "sdm845-mainline";
     repo = "linux";
-    rev = "ecab2cb65fea7355782539d44df7adc2c453d178";
-    hash = "sha256-qNzIkLSUumSZGrdjkNK+R/0+yROrZkM+uNVwrRiNt9I=";
+    rev = "450ba36a9e9f9fb6a1b9773f5fb08cc4fb2ee941";
+    hash = "sha256-LRxlqgZG4HjvsKgaEg9zYv58CbexAssW1qIRb3hOr14=";
   };
 
   isModular = false;
