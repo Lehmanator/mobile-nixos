24dc0d9cfbb9ca79f6b48cc61b99ca5fa793c4d6
Sam Lehman <github@samlehman.dev>
2024-02-14 03:24:59 -0500

    update firmware OnePlus enchilada/fajita

--- devices/oneplus-enchilada/firmware/default.nix
+++ devices/oneplus-enchilada/firmware/default.nix
@@ -1,9 +1,8 @@
-{ lib
-, fetchFromGitLab
-, runCommand
-}:
-
-let
+{
+  lib,
+  fetchFromGitLab,
+  runCommand,
+}: let
   baseFw = fetchFromGitLab {
     owner = "sdm845-mainline";
     repo = "firmware-oneplus-sdm845";
@@ -11,15 +10,14 @@ let
     sha256 = "sha256-bad9CqgRoXx8AyVrGycKQRRcPpPnQy2TsZRBw6u1eIY=";
   };
 in
-runCommand "oneplus-sdm845-firmware"
-{
-  inherit baseFw;
-  # We make no claims that it can be redistributed.
-  meta.license = lib.licenses.unfree;
-} ''
-  mkdir -p $out/lib/firmware
-  cp -r $baseFw/lib/firmware/* $out/lib/firmware/
-  chmod +w -R $out
-  rm -rf $out/lib/firmware/postmarketos
-  cp -r $baseFw/lib/firmware/postmarketos/* $out/lib/firmware
-''
+  runCommand "oneplus-sdm845-firmware" {
+    inherit baseFw;
+    # We make no claims that it can be redistributed.
+    meta.license = lib.licenses.unfree;
+  } ''
+    mkdir -p $out/lib/firmware
+    cp -r $baseFw/lib/firmware/* $out/lib/firmware/
+    chmod +w -R $out
+    rm -rf $out/lib/firmware/postmarketos
+    cp -r $baseFw/lib/firmware/postmarketos/* $out/lib/firmware
+  ''
